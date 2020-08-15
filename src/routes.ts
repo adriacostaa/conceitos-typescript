398e3e21992fdc479769aa946dc11f2047b5f4ec
adriaaCosta <adria.c@outlook.com>
2020-08-15 15:12:59 -0400

    definindo tipos de parametros

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response){
+    const user = createUser('Adria', 'adria@email.com', '12345');
     response.json({ message: 'Hello World'});
 }
---
+++ src/services/CreateUser.ts
@@ -0,0 +1,10 @@
+
+
+export default function createUser(name = '', email: string, password: string){
+    const user = {
+        name,
+        email,
+        password
+    }
+    return user;
+}
