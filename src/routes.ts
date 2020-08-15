f9f20a9d1ebe17dbf0250da84241217eb6041982
adriaaCosta <adria.c@outlook.com>
2020-08-15 15:19:52 -0400

    desestruturacao - definindo tipos nos parametros

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,9 @@ import { Request, Response } from 'express';
 import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response){
-    const user = createUser('Adria', 'adria@email.com', '12345');
+    const user = createUser({
+        email: 'adria@email.com', 
+        password: '12345'
+    });
     response.json({ message: 'Hello World'});
 }
--- src/services/CreateUser.ts
+++ src/services/CreateUser.ts
@@ -1,6 +1,10 @@
 
-
-export default function createUser(name = '', email: string, password: string){
+interface CreateUserData {
+    name?: string;
+    email: string;
+    password: string;
+}
+export default function createUser({name = '', email, password}: CreateUserData){
     const user = {
         name,
         email,
