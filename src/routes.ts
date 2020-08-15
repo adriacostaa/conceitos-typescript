b139c3533268a6b7b8d929e1e61e93c02da7d45b
adriaaCosta <adria.c@outlook.com>
2020-08-15 15:27:07 -0400

    tipagem de vetores

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,13 @@ import createUser from './services/CreateUser';
 export function helloWorld(request: Request, response: Response){
     const user = createUser({
         email: 'adria@email.com', 
-        password: '12345'
+        password: '12345',
+        techs: [
+            'Node.js',
+            'ReactJS',
+            'React Native',
+            { title: 'JavaScript', experience: 100 },
+        ]
     });
     response.json({ message: 'Hello World'});
 }
